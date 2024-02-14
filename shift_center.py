5114acf36ab5300db6e57a085ef18d1218a07bef
RiccardoRoma <64189523+RiccardoRoma@users.noreply.github.com>
2024-02-14 09:22:30 +0100

    cleaned up function

--- shift_center.py
+++ shift_center.py
@@ -1,11 +1,10 @@
 from typing import List
 import numpy as np
-from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister
+from qiskit import QuantumCircuit
 from qiskit.circuit.library import SXGate, SGate
-from generate_star_states import get_leaf_qubits_from_edges
 
 
-def shift_centers(qubit_list: List[int], center_in: int, center_fin: int, circ: QuantumCircuit, add_barries: bool = False) -> QuantumCircuit:
+def shift_centers(qubit_list: List[int], center_in: int, center_fin: int, circ: QuantumCircuit) -> QuantumCircuit:
     # make the two lists of leaf qubits before and after center shift 
     qub_lst_in = qubit_list.remove(center_in)
     qub_lst_fin = qubit_list.remove(center_fin)
@@ -13,6 +12,7 @@ def shift_centers(qubit_list: List[int], center_in: int, center_fin: int, circ: 
     circ.SXGate(center_in)
     for qubit in qub_lst_in:
         circ.SGate(qubit)
+
     # shift to the final center
     circ.SXGate(center_fin)
     for qubit in qub_lst_fin:
