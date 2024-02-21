190af9b025b8d89097a3092594776ac12bb08365
RiccardoRoma <64189523+RiccardoRoma@users.noreply.github.com>
2024-02-21 15:31:21 +0100

    changed with H gates

--- shift_center.py
+++ shift_center.py
@@ -4,18 +4,11 @@ from qiskit import QuantumCircuit
 from qiskit.circuit.library import SXGate, SGate
 
 
-def shift_centers(qubit_list: List[int], center_in: int, center_fin: int, circ: QuantumCircuit) -> QuantumCircuit:
-    # make the two lists of leaf qubits before and after center shift 
-    qub_lst_in = qubit_list.remove(center_in)
-    qub_lst_fin = qubit_list.remove(center_fin)
+def shift_centers(center_in: int, center_fin: int, circ: QuantumCircuit) -> QuantumCircuit:
     # shift away from the initial center
-    circ.SXGate(center_in)
-    for qubit in qub_lst_in:
-        circ.SGate(qubit)
+    circ.h(center_in)
 
     # shift to the final center
-    circ.SXGate(center_fin)
-    for qubit in qub_lst_fin:
-        circ.SGate(qubit)
+    circ.h(center_fin)
     
     return circ
