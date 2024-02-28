426e653bc598ed3bec9debb8149173d7ee42d366
StS-Ph <stephan.j.schuster@gmx.de>
2024-02-28 12:28:26 +0100

    updated star state generation for Graph objects

--- generate_star_states.py
+++ generate_star_states.py
@@ -2,6 +2,8 @@ from typing import Sequence, Tuple
 import numpy as np
 from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister
 import copy
+from networkx import Graph
+import modify_graph_objects as mgo
 
 # This function should return a lists, with all leaf qubits in it
 def get_leaf_qubits_from_edges(edges_in: Sequence[Tuple[int, int]],
@@ -23,7 +25,16 @@ def get_leaf_qubits_from_edges(edges_in: Sequence[Tuple[int, int]],
     #leaf_list = np.asarray(leaf_list)
     return leaf_list
 
-    
+def generate_star_state(G: Graph, circ: QuantumCircuit) -> QuantumCircuit:
+    # assume that circ has one single QuantumRegister with all qubits
+    center_index = mgo.get_graph_center(G)
+
+    # initialize all used qubits in |+>
+    for q in G.nodes():
+        circ.h(q)
+
+    for e in G.edges():
+        circ.cz(e[0], e[1])
 
 def generate_star_states(edges_in: Sequence[Tuple[int, int]],
                          star_centers: Sequence[int],
