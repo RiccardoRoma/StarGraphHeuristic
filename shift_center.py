3abbba149f860d39da61e6f001fab99c139f8b4c
RiccardoRoma <64189523+RiccardoRoma@users.noreply.github.com>
2024-02-28 12:13:27 +0100

    added graph modification

--- shift_center.py
+++ shift_center.py
@@ -1,14 +1,14 @@
-from typing import List
-import numpy as np
 from qiskit import QuantumCircuit
-from qiskit.circuit.library import h
+from networkx import Graph
+from modify_graph_objects import mgo
 
 
-def shift_centers(center_in: int, center_fin: int, circ: QuantumCircuit) -> QuantumCircuit:
+def shift_centers(circ: QuantumCircuit, graph: Graph, center_in: int, center_fin: int) -> QuantumCircuit:
     # shift away from the initial center
     circ.h(center_in)
 
     # shift to the final center
     circ.h(center_fin)
     
-    return circ
+    graph = mgo.update_graph_center(graph, center_fin)
+    return circ, graph
