1f39564f824de1617118a0c5a726e936c59a5a87
StS-Ph <stephan.j.schuster@gmx.de>
2024-02-28 12:58:02 +0100

    added rough structure

--- create_ghz_state_circuit.py
+++ create_ghz_state_circuit.py
@@ -6,3 +6,17 @@ import pickle
 import shift_center
 import generate_star_states
 import merge_graphs
+
+def create_ghz_state_circuit(graph_file: str) -> QuantumCircuit:
+    # load initial graph
+    # run calculate_msq form qkit_input_graph.py
+
+    # ToDo: QuantumCircuit template
+    # iterate through Graph objects and ...
+    # ... call generate star state
+    # ... call shifitng of centers
+    # ... call merging 
+    # ToDo need to know how many ClassicalRegister are needed for merging
+    # Riccardo: we can re-use the classical bit after Pauli corrections
+    return 
+
