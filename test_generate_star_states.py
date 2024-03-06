d83f4b35e29126d72479cd4b3236e220c4e21986
StS-Ph <stephan.j.schuster@gmx.de>
2024-03-06 13:40:17 +0100

    updated test for generate_star_state function

--- test_generate_star_states.py
+++ test_generate_star_states.py
@@ -1,7 +1,31 @@
 import generate_star_states
-edges_list = [(0,2), (1,3), (1,5), (2,5), (2,7), (2,9), (3,6), (3,8), (4,5), (5,9)]
-center_list = [2, 5, 3]
+from Qiskit_input_graph import draw_graph, calculate_msq
+import pickle
+import networkx as nx
+from qiskit import QuantumCircuit
+# test for old version of function 
+# edges_list = [(0,2), (1,3), (1,5), (2,5), (2,7), (2,9), (3,6), (3,8), (4,5), (5,9)]
+# center_list = [2, 5, 3]
+# 
+# c = generate_star_states.generate_star_states(edges_list, center_list, add_barries=True)
 
-c = generate_star_states.generate_star_states(edges_list, center_list, add_barries=True)
-print(c.draw())
+
+fname = "./Saved_small_random_graphs/random_graph_n10_p0.1_erdos_renyi_copy_1.pkl"
+# load initial random graph from pickle file
+G = None
+with open(fname, "rb") as f:
+    G = pickle.load(f)
+
+# Calculate subgraphs and merging sequence
+out = calculate_msq(G)
+subgraphs = out[1]
+
+
+qubit_map = list(G.nodes())
+c = QuantumCircuit(len(G.nodes()))
+generate_star_states.generate_star_state(subgraphs[0], c, qubit_map)
+
+print(c.draw(output="mpl"))
+
+draw_graph(subgraphs[0])
 
