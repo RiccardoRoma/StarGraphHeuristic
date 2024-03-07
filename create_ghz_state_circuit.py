a3488cfb6344d48dc80eebdff8e8f7195b0d2e59
StS-Ph <stephan.j.schuster@gmx.de>
2024-03-07 11:03:35 +0100

    finished implementation of global function

--- create_ghz_state_circuit.py
+++ create_ghz_state_circuit.py
@@ -8,20 +8,23 @@ import generate_star_states
 import merge_graphs
 from Qiskit_input_graph import draw_graph, calculate_msq
 import networkx as nx
+from networkx import Graph
 import modify_graph_objects as mgo
 
-def create_ghz_state_circuit(graph_file: str) -> QuantumCircuit:
-    # load initial graph
+def create_ghz_state_circuit(graph_file: str) -> Tuple[QuantumCircuit, Graph]:
+    # load initial graph from pickle
     G = None
     with open(graph_file, "rb") as f:
         G = pickle.load(f)
 
     # run calculate_msq form qkit_input_graph.py
+        # calculates the list of subgraphs in sequential merging order (MSQ) 
+        # and at what edges we should merge the graphs (merging_edges_list)
     merging_edges_list, MSQ, sid_total_gates = calculate_msq(G, show_status=False)
 
     # set up a QuantumCircuit with all requried qubits and classical bits
-    qregs = QuantumRegister(len(G.nodes()))
-    cregs = ClassicalRegister(len(MSQ)-1) # if merging is sequentially than we need to measure (number of subgraphs -1)-times
+    qregs = QuantumRegister(len(G.nodes())) # number of nodes in initial graph is the number of qubits needed.
+    cregs = ClassicalRegister(len(MSQ)-1) # if merging is sequentially than we need to measure (number of subgraphs - 1)-times
     circ = QuantumCircuit(qregs, cregs)
 
     # create seperate copy for star state generation
@@ -31,20 +34,31 @@ def create_ghz_state_circuit(graph_file: str) -> QuantumCircuit:
         # ... call generate star state
         generate_star_states.generate_star_state(subgraph, circ_substars)
     
-    # create seperate copy for shifting of star centers
-    # ToDo: It seems appropriate to re-arrange the outputs of calculate_msq to make this step easier!
-    #   Iteration should be trough to-be-merged pairs (sG1, sG2) with corr. element of merging_edges_list
-    #   we first call shifting of centers and then call merging function
-    circ_shift = circ.copy()
-    for i in range(len(MSQ)):
-        subgraph = MSQ[i]
-        curr_center = mgo.get_graph_center(subgraph)
-        new_center = 
-        circ_shift, subgraph = shift_center(circ_shift, subgraph, curr_center, )
+    # create seperate copy for shifting of star centers and merging
+    circ_shift_merge = circ.copy()
+    
+    # in each iteration two graphs are center-shifted and then merged
+    subgraph1 = MSQ[0] # start with the first graph in MSQ
+    subgraph2 = None # second graph will be iterated through MSQ
+    cls_bit_cnt = 0 # counts how many measurements have been made
+    for i in range(1,len(MSQ)):
+        # ... call shifting of centers
+        curr_center1 = mgo.get_graph_center(subgraph1) # determine current center
+        new_center1 = merging_edges_list[i-1][0] # get new center
+        circ_shift_merge, subgraph1 = shift_center.shift_centers(circ_shift_merge, subgraph1, curr_center1, new_center1) # call shifting function
 
-    # ... call shifitng of centers
-    # ... call merging 
-    # ToDo need to know how many ClassicalRegister are needed for merging
-    # Riccardo: we can re-use the classical bit after Pauli corrections
-    return 
+        subgraph2 = MSQ[i] # second graph that should be merged with first graph
+        curr_center2 = mgo.get_graph_center(subgraph2) # determine current center
+        new_center2 = merging_edges_list[i-1][1] # get new center
+        circ_shift_merge, subgraph2 = shift_center.shift_centers(circ_shift_merge, subgraph2, curr_center2, new_center2) # call shifting function
+
+        # ... call merging for subgraph1 and subgraph2
+        circ_shift_merge, subgraph1, cls_bit_cnt = merge_graphs.merge_graphs(circ_shift_merge, subgraph1, subgraph2, cls_bit_cnt)
+        
+    # compose circuits
+    circ = circ_substars.compose(circ_shift_merge)
+
+    # ToDo: convert star state into GHZ
+
+    return circ, subgraph1
 
