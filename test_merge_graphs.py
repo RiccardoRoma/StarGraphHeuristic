5aa7efa0514e5c5e4b47a9a736b0fef0a1a28f50
StS-Ph <stephan.j.schuster@gmx.de>
2024-04-17 15:25:18 +0200

    updated test of merge graphs function

--- test_merge_graphs.py
+++ test_merge_graphs.py
@@ -1,44 +1,40 @@
 import merge_graphs
 import shift_center
+import generate_star_states
 from Qiskit_input_graph import draw_graph, calculate_msq
 import pickle
 import networkx as nx
+from networkx import Graph
 from qiskit import QuantumCircuit
 import modify_graph_objects as mgo
-#test for old version of function 
-# edges_list = [(0,2), (1,3), (1,5), (2,5), (2,7), (2,9), (3,6), (3,8), (4,5), (5,9)]
-# center_list = [2, 5, 3]
-# 
-# c = generate_star_states.generate_star_states(edges_list, center_list, add_barries=True)
-# 
-# 
-fname = "./Saved_small_random_graphs/random_graph_n10_p0.1_erdos_renyi_copy_1.pkl"
-# load initial random graph from pickle file
-G = None
-with open(fname, "rb") as f:
-    G = pickle.load(f)
+from qiskit.quantum_info import Statevector
+from qiskit_aer import StatevectorSimulator, AerSimulator
+import numpy as np
+from qiskit.visualization import plot_histogram
+from qiskit.result import marginal_distribution
+import matplotlib.pyplot as plt
 
-# Calculate subgraphs and merging sequence
-merging_edges_list, subgraphs, _ = calculate_msq(G, show_status=False)
+# generate subgraphs and merging sequence
+sub_g0 = Graph()
+sub_g0.add_nodes_from([0,1,2])
+sub_g0.add_edges_from([(0,1),(1,2)])
+
+sub_g1 = Graph()
+sub_g1.add_nodes_from([3,4,5])
+sub_g1.add_edges_from([(3,4),(4,5)])
+subgraphs = [sub_g0, sub_g1]
+
+merging_edges_list=[(1,4)]
 
 print("merging edges list {}".format(merging_edges_list))
 
-
-## Debug
-print("subgraph index 0:")
-print("current graph nodes {}".format(subgraphs[0].nodes))
-draw_graph(subgraphs[0], show=False)
-for i in range(1, len(subgraphs)):
-    print("subgraph index {}:".format(i))
-    print("merge between center {}".format(merging_edges_list[i-1]))
-    print("current graph nodes {}".format(subgraphs[i].nodes))
-    draw_graph(subgraphs[i])
-##
-
-c = QuantumCircuit(len(G.nodes()), 1)
+c = QuantumCircuit(6)
 
 curr_circ = c.copy()
+# handle first subgraph
 curr_graph0 = subgraphs[0]
+# generate corr. star graph state
+curr_circ = generate_star_states.generate_star_state(curr_graph0, curr_circ)
 curr_center0 = mgo.get_graph_center(curr_graph0) # determine current center
 # merging edges list tuples are not ordered after (graph1, graph2) but (smaller value, higher value). Consider this here
 new_center_tuple = merging_edges_list[0]
@@ -48,16 +44,19 @@ elif new_center_tuple[1] in curr_graph0.nodes:
     new_center0 = new_center_tuple[1]
 else:
     raise ValueError("new centers {} don't contain a node of current graph {}".format(new_center_tuple, curr_graph0.nodes))
-draw_graph(curr_graph0, show=False)
-
+# draw_graph(curr_graph0, show=False)
+# check if center must be shifted
 curr_circ, curr_graph0 = shift_center.shift_centers(curr_circ, curr_graph0, curr_center0, new_center0)
 
 
-print(curr_circ.draw(output="mpl"))
+# print(curr_circ.draw(output="mpl"))
 
 draw_graph(curr_graph0, show=False)
 
+# handle second subgraph 
 curr_graph1 = subgraphs[1]
+# generate corr. star graph state
+curr_circ = generate_star_states.generate_star_state(curr_graph1, curr_circ)
 curr_center1 = mgo.get_graph_center(curr_graph1) # determine current center
 # merging edges list tuples are not ordered after (graph1, graph2) but (smaller value, higher value). Consider this here
 new_center_tuple = merging_edges_list[0]
@@ -67,17 +66,50 @@ elif new_center_tuple[1] in curr_graph1.nodes:
     new_center1 = new_center_tuple[1]
 else:
     raise ValueError("new centers {} don't contain a node of current graph {}".format(new_center_tuple, curr_graph1.nodes))
-draw_graph(curr_graph1, show=False)
-
+#draw_graph(curr_graph1, show=False)
+# check if center must be shifted
 curr_circ, curr_graph1 = shift_center.shift_centers(curr_circ, curr_graph1, curr_center1, new_center1)
 
 
-print(curr_circ.draw(output="mpl"))
+#print(curr_circ.draw(output="mpl"))
 
 draw_graph(curr_graph1, show=False)
 
+# merge the two subgraphs
 curr_circ, curr_graph0, _ = merge_graphs.merge_graphs(curr_circ, new_center0, curr_graph0, new_center1, curr_graph1, 0)
 
 print(curr_circ.draw(output="mpl"))
+
+# generate reference of the merged subgraphs
+curr_circ_ref = c.copy()
+# generate star graph state of the merged graph
+curr_circ_ref = generate_star_states.generate_star_state(curr_graph0, curr_circ_ref)
+print(curr_circ_ref.draw(output="mpl"))
+
 draw_graph(curr_graph0)
+
+# compare circuit with reference. Sample 10-times to test both measurement outcomes of the mid-circuit measurement.
+for i in range(10):
+    curr_result = StatevectorSimulator().run(curr_circ).result()
+    curr_state = curr_result.get_statevector(curr_circ)
+    
+    curr_result_ref = StatevectorSimulator().run(curr_circ_ref).result()
+    curr_state_ref = curr_result_ref.get_statevector(curr_circ_ref)
+    
+    overlap = np.abs(curr_state.inner(curr_state_ref))
+    print("run {}: overlap with reference {}".format(i,overlap))
+
+# circ_comb = curr_circ.compose(curr_circ_ref.inverse())
+# circ_comb.measure_all()
+# print(circ_comb.draw(output="mpl"))
+# result = AerSimulator().run(circ_comb).result()
+# 
+# statistics = result.get_counts()
+# #filtered_statistics = marginal_distribution(statistics, [1])
+# #plot_histogram(filtered_statistics)
+# plot_histogram(statistics)
+# filtered_statistics = marginal_distribution(statistics, list(range(1,6)))
+# plot_histogram(filtered_statistics)
+# 
+# plt.show()
 
